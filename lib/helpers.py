ba825f4a4d18c6656f8b1ca3faf21c7ee9f262a7
Jae Jeong <jeongjj29@gmail.com>
2024-09-06 22:12:24 -0400

    add dungeon menu

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,12 +1,35 @@
 # lib/helpers.py
-from models.character import Character
+
+
 
 def start_menu():
     print("Welcome to the adventure game!")
     print("Please enter your name:")
     name = input("> ")
-    Character.create(name)
     print(f"Hello, {name}!")
+
+def dungeon_menu():
+    print("Please select a dungeon.")
+    print("1. Slime Cave")
+    print("2. Spider's Forest")
+    print("3. Undead Graveyard")
+    print("4. Dragon's Den")
+    print("5. Return to the main menu")
+
+    choice = input("> ")
+    if choice == "1":
+        print("You have entered the Slime Cave.")
+    elif choice == "2":
+        print("You have entered the Spider's Forest.")
+    elif choice == "3":
+        print("You have entered the Undead Graveyard.")
+    elif choice == "4":
+        print("You have entered the Dragon's Den.")
+    elif choice == "5":
+        return
+    else:
+        print("Invalid choice. Please try again.")
+
 
 def helper_1():
     print("Performing useful function#1.")
--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,5 +1,4 @@
 import sqlite3
-from character import Character
 
 CONN = sqlite3.connect('company.db')
 CURSOR = CONN.cursor()
